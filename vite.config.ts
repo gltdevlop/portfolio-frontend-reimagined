86baa7cc91e389f923c08db6732abadca4d72e49
gltdevlop <99366409+gltdevlop@users.noreply.github.com>
2024-10-08 14:55:00 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio-frontend-reimagined/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
